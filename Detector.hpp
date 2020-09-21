263cbd785890defbac9d29938e4aae419a49e3ab
ilovecat07 <64274837+ilovecat07@users.noreply.github.com>
2020-09-21 09:50:21 +0800

    Update Detector.hpp

--- Detector.hpp
+++ Detector.hpp
@@ -1,1 +1,19 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
 
+using namespace cv;
+using namespace std;
+
+class Detector
+{
+private:
+
+	Mat _img;
+	vector<Point> _Points;
+
+public:
+
+	void set_img(Mat img);
+	vector<Point> detect();
+	void print();
+};
